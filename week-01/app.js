0b4d35129101c6bc9f9543c6fda8eeb4d4136a0d
Leonardo Albuquerque <leonardo@macrolabs.com.br>
2024-07-07 20:03:32 -0300

    feat: new challenges

--- week-01/app.js
+++ week-01/app.js
@@ -1,22 +1,31 @@
 //Dado um array de objetos que representam pessoas com nome e idade, crie uma função que retorna um novo array contendo apenas os nomes dessas pessoas.
 const getNames = (pessoas) => {};
 
+// Dado um array de objetos que representam eventos com data no formato ISO, crie um novo array de strings formatadas como "DD/MM/YYYY".
 const formatDates = (eventos) => {};
 
+// Dado um array de números, crie um novo array contendo apenas os números maiores que 10.
 const filterGreaterThanTen = (numeros) => {};
 
+// Dado um array de objetos que representam produtos com nome e preço, encontre o primeiro produto que custa mais de 50 unidades monetárias.
 const findExpensiveProduct = (produtos) => {};
 
+// Dado um array de objetos que representam transações com um valor, calcule a soma total dessas transações.
 const calculateTotal = (transacoes) => {};
 
+// Dado um array de strings que representam nomes, crie um novo array onde cada nome está em maiúsculas.
 const toUpperCaseNames = (nomes) => {};
 
+// Dado um array de objetos que representam produtos com nome e preço, aplique um desconto de 10% em cada produto e retorne um novo array com os preços atualizados.
 const applyDiscount = (produtos) => {};
 
+// Dado um array de números representando temperaturas em Celsius, crie um novo array com essas temperaturas convertidas para Fahrenheit.
 const celsiusToFahrenheit = (temperaturas) => {};
 
+// Dado um array de strings que representam nomes, imprima cada nome no console.
 const printNames = (nomes) => {};
 
+// Dado um array de objetos que representam alunos com nome e nota, imprima no console uma mensagem para cada aluno com seu nome e sua nota.
 const printStudentGrades = (alunos) => {};
 
 module.exports = {
